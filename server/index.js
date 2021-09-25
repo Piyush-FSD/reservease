2979574b5f7265f7a78b88c7767bbc8e41103d12
Piyush <piyushpatel.cloud@yahoo.com>
2021-09-25 19:42:32 -0400

    “basic-homepage-user-admin-login-and-registration-endpoints-create-users-admin”

---
+++ server/index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const express = require('express');
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+
+// Handlers
+const { addNewAdmin } = require("./AdminHandlers")
+const { addNewUser } = require('./UserHandlers');
+
+const { Error404 } = require("./ErrorHandler");
+const PORT = 4000;
+
+express()
+    .use(function (req, res, next) {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "OPTIONS, HEAD, GET, PUT, POST, DELETE"
+        );
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        next();
+    })
+    .use(morgan("tiny"))
+    .use(express.static("./server/assets"))
+    .use(bodyParser.json())
+    .use(express.urlencoded({ extended: false }))
+    .use("/", express.static(__dirname + "/"))
+
+    // Register 
+    .post("/register/admin", addNewAdmin)
+    .post("/register/user", addNewUser)
+
+    // ERROR Handler 404 Not Found
+    .get("*", (req, res) => res.status(404).json(Error404))
+
+    .listen(PORT, () => console.info(`Listening on port ${PORT}`));
