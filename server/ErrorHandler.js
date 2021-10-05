606f19185c2ebb927d72cdd9fba2437ab20267d8
Piyush <piyushpatel.cloud@yahoo.com>
2021-10-04 23:16:25 -0400

    remove server

--- server/ErrorHandler.js
+++
@@ -1,7 +0,0 @@
-// Error 404 Handler (Page not Found)
-const Error404 = {
-    status: 404,
-    message: "Oh dear, this endpoint does not exist. Please check your endpoint carefully. In the meantime, enjoy this bacon: 🥓",
-};
-
-module.exports = { Error404 };
